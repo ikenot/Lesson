ae7ca50541e577c20e0d181d8598acbcd775626c
ikenot <139553890+ikenot@users.noreply.github.com>
2023-08-31 14:02:33 +0900

    Add files via upload

--- 5.English/script.js
+++ 5.English/script.js
@@ -1,56 +1,57 @@
 $(function() {
-  $('.main__image').slick({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    fade: true,
-  });
+        $('.main__image').slick({
+          autoplay: true,
+          dots: false,
+          infinite: true,
+          autoplayspeed: 500,
+          fade: true,
+          arrows: false,
+        });
 
-  $( ".header__hamberger--cross" ).click(function() {
-    $( ".header__hamberger--list" ).fadeToggle( function() {
-      $('.header__hamberger--line:nth-child(1)').toggleClass('cross__animation--one');
-      $('.header__hamberger--line:nth-child(2)').toggleClass('cross__animation--two');
-      $('.header__hamberger--line:nth-child(3)').toggleClass('cross__animation--three');      
-    });
-  });
-
-
-  $( ".header__hamberger--child, .header__hamberger--link" ).click(function() {
-    $( ".header__hamberger--list" ).fadeOut( function() {
-      $('.header__hamberger--line:nth-child(1)').removeClass('cross__animation--one');
-      $('.header__hamberger--line:nth-child(2)').removeClass('cross__animation--two');
-      $('.header__hamberger--line:nth-child(3)').removeClass('cross__animation--three');      
+      $( ".header__hamberger--cross" ).click(function() {
+        $( ".header__hamberger--list" ).fadeToggle( function() {
+          $('.header__hamberger--line:nth-child(1)').toggleClass('cross__animation--one');
+          $('.header__hamberger--line:nth-child(2)').toggleClass('cross__animation--two');
+          $('.header__hamberger--line:nth-child(3)').toggleClass('cross__animation--three');      
+        });
       });
-      $('.header__logo--image').attr('src', 'img/logo-r.svg');
-  
-    });  
-  // fadeIn
-  $(window).scroll(function () {
-      var targetElement = $('.reason__content--left').offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > targetElement - windowHeight + 150) {
-        $('.reason__content--left').addClass('scrollin__left');
-      }
-    });
-    $(window).scroll(function () {
-      var targetElement = $('.reason__content--right').offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > targetElement - windowHeight + 150) {
-        $('.reason__content--right').addClass('scrollin__right');
-      }
-    });
-    $(window).scroll(function () {
 
-      $('.voice__item--left, .voice__item--right').each(function() {
-        var targetElement = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > targetElement - windowHeight + 150) {
-          $(this).addClass('zoomin');
-        }
-      });
-    });
-  });   
+
+      $( ".header__hamberger--child, .header__hamberger--link, .header__logo--imagewhite" ).click(function() {
+        $( ".header__hamberger--list" ).fadeOut( function() {
+          $('.header__hamberger--line:nth-child(1)').removeClass('cross__animation--one');
+          $('.header__hamberger--line:nth-child(2)').removeClass('cross__animation--two');
+          $('.header__hamberger--line:nth-child(3)').removeClass('cross__animation--three');      
+          });
+        });
+      
+      // fadeIn
+      $(window).scroll(function () {
+          var targetElement = $('.reason__content--left').offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight) {
+            $('.reason__content--left').addClass('scrollin__left');
+          }
+        });
+        $(window).scroll(function () {
+          var targetElement = $('.reason__content--right').offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight) {
+            $('.reason__content--right').addClass('scrollin__right');
+          }
+        });
+        $(window).scroll(function () {
+
+          $('.voice__item--left, .voice__item--right').each(function() {
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 150) {
+              $(this).addClass('zoomin');
+            }
+          });
+        });
+      });   
 
